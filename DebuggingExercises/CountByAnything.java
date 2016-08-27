d69187652529ee8e0b4d95c97a1b2afd66cf7bfc
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-08-28 09:03:42 +1000

    all tasks complete

---
+++ DebuggingExercises/CountByAnything.java
@@ -0,0 +1,26 @@
+package DebuggingExercises;
+
+/**
+ * Created by jc319816 on 28/08/16.
+ */
+import java.util.Scanner;
+public class CountByAnything
+{
+    public static void main (String args[])
+    {
+        final int STOP = 100;
+        final int NUMBER_PER_LINE;
+        Scanner keyboard = new Scanner(System.in);
+        int stepValue;
+        System.out.print("Enter number to count by >> ");
+        stepValue = keyboard.nextInt();
+        NUMBER_PER_LINE = stepValue * 10;
+        for(int i = stepValue; i <= STOP; i += stepValue)
+        {
+            System.out.print(i + "  ");
+            if(i % NUMBER_PER_LINE == 0)
+                System.out.println();
+        }
+    }
+}
+
---
+++ DebuggingExercises/CountByThrees.java
@@ -0,0 +1,21 @@
+package DebuggingExercises;
+
+/**
+ * Created by jc319816 on 28/08/16.
+ */
+
+public class CountByThrees
+{
+    public static void main (String args[])
+    {
+        final int START = 3;
+        final int STOP = 300;
+        final int NUMBER_PER_LINE = 30;
+        for(int i = START; i <= STOP; i += START)
+        {
+            System.out.print(i + "  ");
+            if(i % NUMBER_PER_LINE == 0)
+                System.out.println();
+        }
+    }
+}
